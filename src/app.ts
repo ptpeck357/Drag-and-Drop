d8ca5b8caf9e2492e25b5bc2f226059ad23173a4
Peter Peck <petertpeck@gmail.com>
2024-04-25 17:56:11 -0600

    add userInput logic.  Add decroators from binding

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+function autobind (
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -5,7 +22,6 @@ class ProjectInput {
     titleInputElement: HTMLFormElement;
     descriptionInputElement: HTMLFormElement;
     peopleInputElement: HTMLFormElement;
-
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -19,7 +35,48 @@ class ProjectInput {
         this.descriptionInputElement = this.element.querySelector('#description')! as HTMLFormElement;
         this.peopleInputElement = this.element.querySelector('#people')! as HTMLFormElement;
 
+        this.configure();
         this.attach();
+    }
+
+    private gatherUserInput():[string, string, number] | void {
+        const enterTitle = this.titleInputElement.value;
+        const enterdescription = this.descriptionInputElement.value;
+        const enterPeople = this.peopleInputElement.value;
+
+        if(
+            enterTitle.trim().length === 0 ||
+            enterdescription.trim().length === 0 ||
+            enterPeople.trim().length === 0
+        ){
+            alert("please try again");
+            return;
+        }
+        else {
+            return [enterTitle, enterPeople, +enterPeople];
+        }
+    }
+
+    private clearInputs(){
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
+    private submitHandler(event: Event){
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+
+        if(Array.isArray(userInput) ){
+            const [title, desc, people] = userInput
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
